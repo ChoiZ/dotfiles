40cd88a2f03a7534dbabae8c4fa76b25710e7868
François LASSERRE <choiz@me.com>
2016-05-27 17:59:59 +0200

    Add alias for vim, python, ip

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,8 @@
 #/usr/bin/zsh
 
-alias tmux="tmux -2"
-alias lo="xscreensaver-command -lock"
+alias v=vim
+alias vi=vim
+alias py=python
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias git_branch="~/.dotfiles/git/git_branch.sh"
 alias pullrequest="~/.dotfiles/git/pullrequest.sh"
